9ce73c906c43f024f867096801e230728e0e34ee
Abdelmoneim Desouki <31845116+abdoudesouki@users.noreply.github.com>
2022-03-28 20:22:04 +0200

    Update csv2ttl.py

--- csv2ttl.py
+++ csv2ttl.py
@@ -27,56 +27,52 @@ g = Graph()
 
 qvoc= Namespace("http://www.nlp2rdf.org/quranvocab#")
 gold= Namespace("http://purl.org/linguistics/gold/")
-cvdr = Namespace("https://covid-19ds.data/resource/")  #(for Subject/Resource)
-lgdo = Namespace("http://linkedgeodata.org/page/ontology/")
-vcard = Namespace("http://www.w3.org/2006/vcard/ns#")
+lexvo= Namespace("http://lexvo.org/id/iso639-3/")
+semq= Namespace("http://mlode.nlp2rdf.org/semanticquran/quran")
 dbpprop = Namespace("http://dbpedia.org/property/")
 dbpediaOwl = Namespace("http://dbpedia.org/ontology/")
 
 
 
-g.namespace_manager.bind("cvdo", cvdo)
-g.namespace_manager.bind("cvdr", cvdr)
-g.namespace_manager.bind("lgdo", lgdo)
-g.namespace_manager.bind("vcard", vcard)
+g.namespace_manager.bind("qvoc", qvoc)
+g.namespace_manager.bind("semq", semq)
+#g.namespace_manager.bind("cvdr", cvdr)
+#g.namespace_manager.bind("lgdo", lgdo)
+#g.namespace_manager.bind("vcard", vcard)
 
 
 # Load the CSV data as a pandas Dataframe.
-csv_data = pd.read_csv("D:/dice/acaps_covid19_government_measures_dataset_.csv")
+csv_data = pd.read_csv("D:\\Dropbox\\Web\\SemanticQuran\\chapters.tsv",sep='\t',encoding='utf8')
 
 print(csv_data.head())
 print(csv_data.count())
 
 #%%
-print(csv_data.loc[0]['ID'])
+print(csv_data.loc[0]['sw_id'])
 #%%
-# Here I deal with spaces (" ") in the data. I replace them with "_" so that URI's become valid.
-#csv_data = csv_data.replace(to_replace=" ", value="_", regex=True)
-csv_data["COUNTRY"]=csv_data["COUNTRY"].str.replace(" ","_")
-csv_data["REGION"]=csv_data["REGION"].str.replace(" ","_")
-
-# Here I mark all missing/empty data as "unknown". This makes it easy to delete triples containing this later.
-#csv_data = csv_data.fillna("unknown")
 
 # Loop through the CSV data, and then make RDF triples.
 for index, row in csv_data.iterrows():
-#for row in csv_data:
-    # The names of the people act as subjects.
-   # subject = row['Name'] #### I have made it commented #######
-     #subjects = row["ID"]
-    # Create triples: e.g. "Cade_Tracey - age - 27"
-    #print(cvdr[str(row['ID'])], row['ISO'])
     
-     g.add((URIRef(cvdr[str(row["ID"])]), URIRef(cvdo.hasISO), URIRef(cvdr[row["ISO"]])))
-     
-     g.add((URIRef(cvdr[str(row["ID"])]), URIRef(cvdo.hasCountry), URIRef(cvdr[row["COUNTRY"]])))
+     g.add((URIRef(semq[str(row["sw_id"])]), SKOS.prefLabel,Literal(row["chnamear"], datatype=XSD.string) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), RDFS.label,Literal(row["chnamear"], datatype=XSD.string) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), RDF.type,URIRef(qvoc+"Chapter") ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.verseCount),Literal(row["cntVerse"], datatype=XSD.nonNegativeInteger) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.chapterIndex),Literal(row["sw_id"], datatype=XSD.nonNegativeInteger) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.inPage),Literal(row["PageNo"], datatype=XSD.nonNegativeInteger) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.inPart),Literal(row["PartNo"], datatype=XSD.nonNegativeInteger) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.inQuarter),Literal(row["Rob3"], datatype=XSD.nonNegativeInteger) ))
+     g.add((URIRef(semq[str(row["sw_id"])]), URIRef(qvoc.inSection),Literal(row["HezbNo"], datatype=XSD.nonNegativeInteger) ))
     
-     g.add((URIRef(cvdr[str(row["COUNTRY"])]), URIRef(cvdo.countryName), Literal(row["COUNTRY"], datatype=XSD.string)))
-    
-     g.add((URIRef(cvdr[str(row["ID"])]), URIRef(cvdo.hasRegion), Literal(row["REGION"])))
 
-    #g.add((URIRef(ex + subject), URIRef(ex + "married"), URIRef(ex + row["Spouse"])))
-    #g.add((URIRef(ex + subject), URIRef(ex + "country"), URIRef(ex + row["Country"])))
+
+    # g.add((URIRef(cvdr[str(row["ID"])]), URIRef(cvdo.hasCountry), URIRef(cvdr[row["COUNTRY"]])))
+    
+     #g.add((URIRef(cvdr[str(row["COUNTRY"])]), URIRef(cvdo.countryName), Literal(row["COUNTRY"], datatype=XSD.string)))
+    
+     #g.add((URIRef(cvdr[str(row["ID"])]), URIRef(cvdo.hasRegion), Literal(row["REGION"])))
+
+
 
     # If We want can add additional RDF/RDFS/OWL information e.g
     #g.add((URIRef(ex + subject), RDF.type, FOAF.Person))
@@ -86,4 +82,4 @@ for index, row in csv_data.iterrows():
 
 # Clean printing of the graph.
 #print(g.serialize(format="turtle").decode())
-g.serialize(destination='my_graph.ttl', format='ttl')
+g.serialize(destination='semq.ttl', format='ttl')
